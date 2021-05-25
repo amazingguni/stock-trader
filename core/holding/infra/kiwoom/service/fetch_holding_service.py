0aa4d7d3cc92f2af405ab6a5f2ef5297b04a1586
amazingguni <amazingguni@gmail.com>
2021-05-25 22:22:23 +0900

    Finish implement fetch holding service

--- core/holding/infra/kiwoom/service/fetch_holding_service.py
+++ core/holding/infra/kiwoom/service/fetch_holding_service.py
@@ -1,6 +1,6 @@
 from core.external.kiwoom import OpenApiClient, InputValue
 
-from core.holding.domain import HoldingSummary
+from core.holding.domain import HoldingSummary, Holding
 from core.holding.domain.service import FetchHoldingService
 
 
@@ -29,4 +29,26 @@ class KiwoomFetchHoldingService(FetchHoldingService):
         return None
 
     def fetch_stocks(self, account_number):
-        pass
+        input_values = [
+            InputValue(s_id='계좌번호', s_value=account_number),
+        ]
+        row_keys = ['종목번호', '종목명', '보유수량', '매입가',
+                    '현재가', '평가손익',  '수익률(%)', '매입금액']
+        trcode = 'opw00018'
+        response = self.openapi_client.comm_rq_data_repeat(
+            trcode, input_values, row_keys)
+
+        holdings = []
+        for row in response.rows:
+            holding = Holding(
+                account_number=account_number,
+                stock_code=row['종목번호'],
+                stock_name=row['종목명'],
+                quantity=int(row['보유수량']),
+                purchase_price=int(row['매입가']),
+                current_price=int(row['현재가']),
+                eval_profit_loss_price=int(row['평가손익']),
+                earning_rate=float(row['수익률(%)']),
+                total_purchase_price=int(row['매입금액']))
+            holdings.append(holding)
+        return holdings
--- tests/holding/infra/test_kiwoom_fetch_holding_service.py
+++ tests/holding/infra/test_kiwoom_fetch_holding_service.py
@@ -15,6 +15,13 @@ def test_fetch_summary(openapi_client, kiwoom_account):
     assert holding_summary
 
 
+@pytest.mark.kiwoom
+def test_fetch_stocks(openapi_client, kiwoom_account):
+    # When
+    KiwoomFetchHoldingService(
+        openapi_client).fetch_stocks(kiwoom_account.number)
+
+
 def test_mock_fetch_summary():
     mock_client = mock.MagicMock()
     mock_client.comm_rq_single_data.return_value = RequestResponse(
@@ -32,4 +39,40 @@ def test_mock_fetch_summary():
     assert pytest.approx(91.1, 1e-9) == holding_summary.total_earning_rate
     assert holding_summary.estimated_deposit == 10001
 
-# def test_mock_fetch_stocks():
+
+def test_mock_fetch_stocks():
+    mock_client = mock.MagicMock()
+    mock_client.comm_rq_data_repeat.return_value = RequestResponse(
+        rows=[{
+            '종목번호': "111111",
+            '종목명': "삼성전자",
+            '보유수량': 10,
+            '매입가': 10000,
+            '현재가': 9000,
+            '평가손익': -1000,
+            '수익률(%)': -10,
+            '매입금액': 100000,
+        }, {
+            '종목번호': "222222",
+            '종목명': "우리은행",
+            '보유수량': 3,
+            '매입가': 10,
+            '현재가': 11,
+            '평가손익': 1,
+            '수익률(%)': 10.1,
+            '매입금액': 30,
+        }])
+    holdings = KiwoomFetchHoldingService(
+        mock_client).fetch_stocks('11111111')
+
+    assert len(holdings) == 2
+    assert holdings[0].stock_code == '111111'
+    assert holdings[0].stock_name == '삼성전자'
+    assert holdings[0].account_number == '11111111'
+    assert holdings[0].quantity == 10
+
+    assert holdings[1].purchase_price == 10
+    assert holdings[1].current_price == 11
+    assert holdings[1].eval_profit_loss_price == 1
+    assert holdings[1].earning_rate == pytest.approx(10.1, 1e-9)
+    assert holdings[1].total_purchase_price == 30
